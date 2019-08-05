5b099e84f41a7892ccc4648b2f3684590cdd5c43
datapsyche <jithinjkumar@gmail.com>
2019-08-05 10:25:21 +0530

    Added NodeJS

---
+++ person.js
@@ -0,0 +1,17 @@
+const person = {
+    name: 'John',
+    age: 28
+}
+
+class Person {
+    constructor(name,age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting() {
+        console.log(`My Name is ${this.name} and I'm ${this.age}`);
+    }
+}
+module.exports = Person; 
+
